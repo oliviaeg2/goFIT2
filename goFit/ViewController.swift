efa2b8d1fc7311d0d9fd14d8630c1e8a50ef4e8d
SwiftHacker <drussu@stanford.edu>
2017-11-13 00:06:54 -0800

    Initial Commit

---
+++ goFit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goFit
+//
+//  Created by Denis Russu on 11/13/17.
+//  Copyright © 2017 CS147. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
