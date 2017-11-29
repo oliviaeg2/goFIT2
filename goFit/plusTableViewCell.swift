b56e2f4b5507f0d1176dbd6490e39f8479b30e6a
SwiftHacker <drussu@stanford.edu>
2017-11-29 12:25:53 -0800

    first commit

---
+++ goFit/plusTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  plusTableViewCell.swift
+//  goFit
+//
+//  Created by Denis Russu on 11/28/17.
+//  Copyright © 2017 CS147. All rights reserved.
+//
+
+import UIKit
+
+class plusTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
