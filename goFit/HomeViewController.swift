6eb1f6abe46b794e4c14b3496ab0c84e525002e9
Olivia Gregory <oliviagregory@Olivias-MacBook-Pro-3.local>
2017-12-04 15:11:43 -0800

    Logging from home screen

--- goFit/HomeViewController.swift
+++ goFit/HomeViewController.swift
@@ -18,22 +18,38 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var progress: UILabel!
     @IBOutlet weak var goal: UILabel!
     
+    @IBOutlet weak var currentChallengeLabel: UILabel!
+    
+    var currentInt = 0;
+    var progressVal = 2;
+    var goalVal = 4;
+    
     @IBAction func minus() {
-        let progressValue = Int(progress.text!)!
-        if progressValue > 0 {
-            progress.text = String(progressValue - 1)
-        }
-    }
+     }
     
     @IBAction func plus() {
-        let progressValue = Int(progress.text!)!
-        if progressValue < Int(goal.text!)! {
-            progress.text = String(progressValue + 1)
+//        let progressValue = Int(progress.text!)!
+//        if progressValue < Int(goal.text!)! {
+//            progress.text = String(progressValue + 1)
+//        }
+        let curChallenge = Challenge.userChallengesShared[currentInt]
+        if (curChallenge.progress < curChallenge.frequency) {
+          curChallenge.progress += 1
         }
+        progress.text = String(curChallenge.progress)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newChallenge = Challenge.userChallengesShared[currentInt]
+        currentChallengeLabel.text = newChallenge.type;
+        currentChallenge.image = newChallenge.icon!
+        progressVal = newChallenge.progress
+        goalVal = newChallenge.frequency
+        progress.text = String(progressVal)
+        goal.text = String(goalVal)
+        
         
         currentChallenge.layer.cornerRadius = currentChallenge.frame.size.width / 2;
         currentChallenge.clipsToBounds = true;
@@ -41,6 +57,34 @@ class HomeViewController: UIViewController {
         currentChallenge.layer.borderWidth = 10.0;
         currentChallenge.layer.borderColor = UIColor.green.cgColor
         
+    }
+    
+    @IBAction func touchedPrevious(_ sender: Any) {
+        if (currentInt - 1 >= 0) {
+            currentInt-=1;
+            let newChallenge = Challenge.userChallengesShared[currentInt]
+            currentChallengeLabel.text = newChallenge.type;
+            currentChallenge.image = newChallenge.icon!
+            progressVal = newChallenge.progress
+            goalVal = newChallenge.frequency
+            progress.text = String(progressVal)
+            goal.text = String(goalVal)
+        }
+    }
+    
+    @IBAction func touchedNext(_ sender: Any) {
+      
+        if (currentInt + 1 < Challenge.userChallengesShared.count) {
+        currentInt+=1;
+        let newChallenge = Challenge.userChallengesShared[currentInt]
+        currentChallengeLabel.text = newChallenge.type;
+        currentChallenge.image = newChallenge.icon!
+        progressVal = newChallenge.progress
+        goalVal = newChallenge.frequency
+        progress.text = String(progressVal)
+        goal.text = String(goalVal)
+
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
